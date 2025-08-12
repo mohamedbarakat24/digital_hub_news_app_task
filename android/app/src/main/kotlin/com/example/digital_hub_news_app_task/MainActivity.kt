e321beef251a6e0dd285c09d6bf596690f049135
mohamedbarakat24 <122083895+mohamedbarakat24@users.noreply.github.com>
2025-08-12 18:14:01 +0300

    intial commit

---
+++ android/app/src/main/kotlin/com/example/digital_hub_news_app_task/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.digital_hub_news_app_task
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(),
+      home: Scaffold(
+        appBar: AppBar(title: const Text("Hello, World!")),
+        body: const Center(child: Text("Hello, World!")),
+      ),
+    );
+  }
+}
